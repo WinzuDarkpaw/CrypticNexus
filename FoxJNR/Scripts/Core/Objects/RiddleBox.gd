3b8b1c2e679aef60586b1d55ee2f89caf7b77f1f
Winzu <niconowak3@gmail.com>
2022-09-16 21:02:25 +0200

    Boxwerfen gefixt, funzt

--- FoxJNR/Scripts/Core/Objects/RiddleBox.gd
+++ FoxJNR/Scripts/Core/Objects/RiddleBox.gd
@@ -1,6 +1,9 @@
 class_name RiddleBox
 extends KinematicBody2D
 
+# Laden eines Spielerobjekts.
+var obj = load("res://Scripts/Core/Objects/Player.gd")
+
 # Behandelt das Verhalten von beweglichen Boxen.
 var boxTexture
 var isThrowable = false
@@ -15,18 +18,16 @@ func set_texture(texture):
 	$Sprite.texture = boxTexture
 
 func _physics_process(delta):
+	print(isThrowable)
 	# Die Geschwindigkeit wird nicht steigen, wenn sich die Box auf dem Boden
 	# befindet.
 	if is_on_floor():
 		boxVelocity.y = 0
-	
 	if isThrowable and Input.is_action_just_pressed("Interact"):
 		boxVelocity.y -= 160
 	# Befindet sich die Box nicht auf dem Boden, soll diese fallen.
 	else:
 		boxVelocity.y += gravity
-	
-	
 	
 	move_and_slide(boxVelocity, Vector2.UP)
 
@@ -34,10 +35,10 @@ func _physics_process(delta):
 func push(velocity: Vector2) -> void:
 	move_and_slide(velocity, Vector2())
 
-
 func _on_InteractableIndicator_body_entered(body):
-	isThrowable = true
-
+	if body is obj:
+		isThrowable = true
 
 func _on_InteractableIndicator_body_exited(body):
-	isThrowable = false
+	if body is obj:
+		isThrowable = false
